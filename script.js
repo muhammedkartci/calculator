7fe1e220d43bbe126fbedbb083889ce5f8e3ed14
Muhammed Fatih Kartcı <muhammedfatihkartci@Muhammed-MacBook-Pro.local>
2022-08-29 15:26:45 +0300

    formiller eklendi

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 class Calculator {
 
     constructor(previousOperandTextElement, currentOperandTextElement){
-        this.previousOperandTextElement = currentOperandTextElement
-        this.currentOperandTextElement = previousOperandTextElement
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
         this.clear()
     }
     clear() {
@@ -14,16 +14,47 @@ class Calculator {
 
     }
     appendNumber(number) {
-    
+        if (number  === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
     }
     chooseOperation(operation) {
-
-    }
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand= ''
+        }
     compute() {
-
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break;
+            case '-':
+                computation = prev - current
+                break;
+            case 'x':
+                    computation = prev * current
+                    break;
+            case '÷':
+                        computation = prev / current
+                    break;
+        
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
     }
     updateDisplay() {
-       
+       this.currentOperandTextElement.innerText = this.currentOperand
+       this.previousOperandTextElement.innerText = this.previousOperand
     }
 }
 
@@ -46,4 +77,15 @@ numberButtons.forEach(button => {
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
     })
-} )
+})
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+equalsButtons.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+}) 
+
